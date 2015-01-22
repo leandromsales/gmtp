7c2fb6808faace82e7c9e9276f85b32390f156a4
Wendell <wendell@wendell-Elementary-II.(none)>
2015-01-22 15:07:08 -0300

    Update server to get IP address automatically

--- app/python/client.py
+++ app/python/client.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python
 
 import socket
+import fcntl
+import struct
+import time
+
+def get_ip_address(ifname):
+	a = 'b'
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s', ifname[:15]))[20:24])
 
 IPPROTO_GMTP = 254
 
-address = (socket.gethostname(), 12345)
-#address = ("192.168.1.108", 12345)
+ip_address = get_ip_address('wlan0')
+#ip_address = "172.20.9.89"
+
+address = (ip_address, 12345)
 
 # Create sockets
 client_socket = socket.socket(socket.AF_INET, 7, IPPROTO_GMTP)
