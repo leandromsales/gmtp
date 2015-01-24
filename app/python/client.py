b1d48929b49233dc5d4c52bed823c91fca710ec1
Wendell Silva Soares <wensiso2000@gmail.com>
2015-01-23 21:27:42 -0300

    Update client.py and server.py

--- app/python/client.py
+++ app/python/client.py
@@ -6,16 +6,22 @@ import struct
 import time
 
 def get_ip_address(ifname):
-	a = 'b'
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s', ifname[:15]))[20:24])
+    a = 'b'
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s', ifname[:15]))[20:24])
 
 IPPROTO_GMTP = 254
 
-ip_address = get_ip_address('eth0')
-#ip_address = "172.20.9.89"
+default_ip = get_ip_address('eth0')
+default_address = (ip_default, 12345)
 
-address = (ip_address, 12345)
+print(sys.argv)
+if(len(sys.argv) > 2):
+    address = (sys.argv[1], int(sys.argv[2]))
+elif(len(sys.argv) > 1):
+    address = (sys.argv[1], 12345)
+else:
+    address = default_address
 
 # Create sockets
 client_socket = socket.socket(socket.AF_INET, 7, IPPROTO_GMTP)
@@ -25,8 +31,8 @@ client_socket.connect(address)
 
 # Echo
 while True:
-	text = input("Informe texto ou digite 'sair' para desconectar: ")
-	client_socket.send(text.encode('utf-8'))
-	if (text == "sair"):
-		client_socket.close()
-		break
+    text = input("Informe texto ou digite 'sair' para desconectar: ")
+    client_socket.send(text.encode('utf-8'))
+    if (text == "sair"):
+        client_socket.close()
+        break
--- app/python/server.py
+++ app/python/server.py
@@ -13,13 +13,12 @@ def get_ip_address(ifname):
 SOCK_GMTP = 7
 IPPROTO_GMTP = 254
 
-ip_address = get_ip_address('eth0')
+ip = get_ip_address('eth0')
 
-print("Starting server... at ", ip_address)
-address = (ip_address, 12345)
+print("Starting server... at ", ip)
+address = (ip, 12345)
 
 # Create sockets
-#server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.getprotobyname("gmtp"))
 server_socket = socket.socket(socket.AF_INET, SOCK_GMTP, IPPROTO_GMTP)
 print(server_socket)
 
