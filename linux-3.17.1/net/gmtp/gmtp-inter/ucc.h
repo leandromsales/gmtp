94683866aa4346da273921d7f23424cb94aa3d87
Wendell Silva Soares <wensiso2000@gmail.com>
2015-05-14 03:12:48 -0300

    Fix name of GMTP-Inter module and sources
    Change from GMTP-Intra to GMTP-Inter. GMTP-Intra is already GMTP module itself...
    GMTP-Inter is a netfilter module that emulates a router (GMTP Relay)

---
+++ linux-3.17.1/net/gmtp/gmtp-inter/ucc.h
@@ -0,0 +1,18 @@
+/*
+ * ucc.h
+ *
+ *  Created on: 14/05/2015
+ *      Author: wendell
+ */
+
+#ifndef UCC_H_
+#define UCC_H_
+
+/** gmtp-ucc. */
+unsigned int gmtp_rtt_average(void);
+unsigned int gmtp_rx_rate(void);
+unsigned int gmtp_relay_queue_size(void);
+unsigned int gmtp_get_current_rx_rate(void);
+void gmtp_update_rx_rate(unsigned int h_user);
+
+#endif /* UCC_H_ */
--- linux-3.17.1/net/gmtp/html/search/all_70.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['pra_5ftestar',['pra_testar',['../classpra__testar.html',1,'']]],
-  ['proto_2ec',['proto.c',['../proto_8c.html',1,'']]]
-];
