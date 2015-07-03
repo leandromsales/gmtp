fb84f78a20d0ec1004e331df4efab23f856b4118
Wendell Silva Soares <wensiso2000@gmail.com>
2015-07-03 01:27:51 -0300

    Configuring GMTP-Inter module in Ns-3 simulation

---
+++ app/ns-3-dce/example/gmtp.h
@@ -0,0 +1,36 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define SOCK_GMTP     7
+#define IPPROTO_GMTP  254
+#define SOL_GMTP      281
+
+enum gmtp_sockopt_codes {
+	GMTP_SOCKOPT_FLOWNAME = 1,
+	GMTP_SOCKOPT_MAX_TX_RATE,
+	GMTP_SOCKOPT_GET_CUR_MSS,
+	GMTP_SOCKOPT_SERVER_RTT,
+	GMTP_SOCKOPT_SERVER_TIMEWAIT,
+	GMTP_SOCKOPT_PULL,
+	GMTP_SOCKOPT_ROLE_RELAY,
+	GMTP_SOCKOPT_RELAY_ENABLED
+};
+
+void set_gmtp_inter(int actived)
+{
+  int ok = 1;
+  int rsock = socket (PF_INET, SOCK_GMTP, IPPROTO_GMTP);
+  setsockopt(rsock, SOL_GMTP, GMTP_SOCKOPT_ROLE_RELAY, &ok, sizeof(int));
+  setsockopt(rsock, SOL_GMTP, GMTP_SOCKOPT_RELAY_ENABLED, &actived, sizeof(int));
+  close(rsock);
+}
+
+inline void disable_gmtp_inter()
+{
+    set_gmtp_inter(false);
+}
+
+inline void enable_gmtp_inter()
+{
+    set_gmtp_inter(true);
+}
