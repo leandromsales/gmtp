f4db37828709954aade2150ec1bacf24d9cb380b
Joilnen Leite <joilnen@gmail.com>
2015-07-07 20:17:23 -0300

    initial versio of select code

--- linux-4.0.3/net/gmtp/proto.c
+++ linux-4.0.3/net/gmtp/proto.c
@@ -10,6 +10,7 @@
 
 #include <uapi/linux/gmtp.h>
 #include <linux/gmtp.h>
+#include <linux/poll.h>
 #include "gmtp.h"
 #include "mcc.h"
 
@@ -75,7 +76,7 @@ const char *gmtp_state_name(const int state)
 	[GMTP_OPEN]		= "OPEN",
 	[GMTP_REQUESTING]	= "REQUESTING",
 	[GMTP_LISTEN]		= "LISTEN",
-	[GMTP_REQUEST_RECV]		= "REQUEST/REGISTER_RECEIVED",
+	[GMTP_REQUEST_RECV]     = "REQUEST/REGISTER_RECEIVED",
 	[GMTP_ACTIVE_CLOSEREQ]	= "CLOSEREQ",
 	[GMTP_PASSIVE_CLOSE]	= "PASSIVE_CLOSE",
 	[GMTP_CLOSING]		= "CLOSING",
@@ -599,6 +600,55 @@ out:
 }
 
 EXPORT_SYMBOL_GPL(gmtp_recvmsg);
+
+unsigned int gmtp_poll(struct file *file, struct socket *sock,
+                poll_table *wait) {
+	unsigned int mask;
+	struct sock *sk = sock->sk;
+
+	sock_poll_wait(file, sk_sleep(sk), wait);
+	if (sk->sk_state == GMTP_LISTEN)
+		return inet_csk_listen_poll(sk);
+
+	/* Socket is not locked. We are protected from async events
+	   by poll logic and correct handling of state changes
+	   made by another threads is impossible in any case.
+	 */
+
+	mask = 0;
+	if (sk->sk_err)
+		mask = POLLERR;
+
+	if (sk->sk_shutdown == SHUTDOWN_MASK || sk->sk_state == GMTP_CLOSED)
+		mask |= POLLHUP;
+	if (sk->sk_shutdown & RCV_SHUTDOWN)
+		mask |= POLLIN | POLLRDNORM | POLLRDHUP;
+
+	/* Connected? */
+	if ((1 << sk->sk_state) & ~(GMTPF_REQUESTING)) {
+		if (atomic_read(&sk->sk_rmem_alloc) > 0)
+			mask |= POLLIN | POLLRDNORM;
+
+		if (!(sk->sk_shutdown & SEND_SHUTDOWN)) {
+			if (sk_stream_is_writeable(sk)) {
+				mask |= POLLOUT | POLLWRNORM;
+			} else {  /* send SIGIO later */
+				set_bit(SOCK_ASYNC_NOSPACE,
+					&sk->sk_socket->flags);
+				set_bit(SOCK_NOSPACE, &sk->sk_socket->flags);
+
+				/* Race breaker. If space is freed after
+				 * wspace test but before the flags are set,
+				 * IO signal will be lost.
+				 */
+				if (sk_stream_is_writeable(sk))
+					mask |= POLLOUT | POLLWRNORM;
+			}
+		}
+	}
+	return mask;
+}
+EXPORT_SYMBOL_GPL(gmtp_poll);
 
 int gmtp_sendmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 		size_t len)
