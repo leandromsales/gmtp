61a62b91ac2e4986fc80f14278dd1d558fc8a21a
Wendell Silva Soares <wensiso2000@gmail.com>
2015-07-07 23:20:39 -0300

    Addding select client-server example

--- app/ns-3-dce/example/server.cc
+++ app/ns-3-dce/example/server.cc
@@ -1,90 +1,114 @@
-#include "stdio.h"  
-#include "stdlib.h"  
-#include "sys/types.h"  
-#include "sys/socket.h"  
-#include "string.h"  
-#include "netinet/in.h"  
+#include "stdio.h"
+#include "stdlib.h"
+#include "sys/types.h"
+#include "sys/socket.h"
+#include "string.h"
+#include "netinet/in.h"
 #include "unistd.h"
 #include "arpa/inet.h"
-  
-#define BUF_SIZE 2000  
-#define CLADDR_LEN 100  
+
+#include "gmtp.h"
+
+#define BUF_SIZE 2000
+#define CLADDR_LEN 100
 
 #define PORT 2000
 
-#define SOCK_GMTP     7
-#define IPPROTO_GMTP  254
-#define SOL_GMTP      281
-  
-int main() {  
- struct sockaddr_in addr, cl_addr;  
- int sockfd, ret, newsockfd; 
- socklen_t len;
- char buffer[BUF_SIZE];  
- pid_t childpid;  
- char clientAddr[CLADDR_LEN];  
-   
- sockfd = socket(AF_INET, SOCK_GMTP, IPPROTO_GMTP);  
- setsockopt(sockfd, SOL_GMTP, 1, "1234567812345678", 16);
- if (sockfd < 0) {  
-  printf("Error creating socket!\n");  
-  exit(1);  
- }  
- printf("Socket created...\n");  
-   
- memset(&addr, 0, sizeof(addr));  
- addr.sin_family = AF_INET;  
- addr.sin_addr.s_addr = INADDR_ANY;  
- addr.sin_port = PORT;  
-   
- ret = bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));  
- if (ret < 0) {  
-  printf("Error binding!\n");  
-  exit(1);  
- }  
- printf("Binding done...\n");  
-  
- printf("Waiting for a connection...\n");  
- listen(sockfd, 5);  
-  
- for (;;) { //infinite loop  
-  len = sizeof(cl_addr);  
-  newsockfd = accept(sockfd, (struct sockaddr *) &cl_addr, &len);  
-  if (newsockfd < 0) {  
-   printf("Error accepting connection!\n");  
-   exit(1);  
-  }  
-  printf("Connection accepted...\n");  
-  
-  inet_ntop(AF_INET, &(cl_addr.sin_addr), clientAddr, CLADDR_LEN);  
-  if ((childpid = fork()) == 0) { //creating a child process  
-  
-   close(sockfd);   
-//stop listening for new connections by the main process.   
-//the child will continue to listen.   
-//the main process now handles the connected client.  
-  
-   for (;;) {  
-    memset(buffer, 0, BUF_SIZE);  
-    ret = recvfrom(newsockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &cl_addr, &len);  
-    if(ret < 0) {  
-     printf("Error receiving data!\n");    
-     exit(1);  
-    }  
-    printf("Received data from %s: %s\n", clientAddr, buffer);   
-  
-    ret = sendto(newsockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &cl_addr, len);     
-    if (ret < 0) {    
-     printf("Error sending data!\n");    
-     exit(1);    
-    }    
-    printf("Sent data to %s: %s\n", clientAddr, buffer);  
-   }  
-  }  
-  close(newsockfd);  
- }  
+void handle(int newsock, fd_set *set);
 
- return 0;
-}  
+int main()
+{
+	struct sockaddr_in addr, cl_addr;
+	int sockfd, ret;
+	socklen_t len;
+	fd_set socks;
+	fd_set readsocks;
+	int maxsock;
+	int reuseaddr = 1;
 
+	disable_gmtp_inter();
+	sockfd = socket(AF_INET, SOCK_GMTP, IPPROTO_GMTP);
+	setsockopt(sockfd, SOL_GMTP, 1, "1234567812345678", 16);
+//	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sockfd < 0) {
+		printf("Error creating socket!\n");
+		exit(1);
+	}
+	printf("Socket created...\n");
+
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons (PORT);
+
+	/* Enable the socket to reuse the address */
+	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr,
+			sizeof(int))) {
+		perror("setsockopt");
+		return 1;
+	}
+
+	ret = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+	if(ret < 0) {
+		printf("Error binding!\n");
+		exit(1);
+	}
+	printf("Binding done...\n");
+
+	printf("Waiting for a connection...\n");
+	listen(sockfd, 5);
+
+	/* Set up the fd_set */
+	FD_ZERO(&socks);
+	FD_SET(sockfd, &socks);
+	maxsock = sockfd;
+
+	/* Main loop */
+	while(1) {
+	        unsigned int s;
+	        readsocks = socks;
+	        if (select(maxsock + 1, &readsocks, NULL, NULL, NULL) == -1) {
+	            perror("select");
+	            return 1;
+	        }
+	        for (s = 0; s <= maxsock; s++) {
+	            if (FD_ISSET(s, &readsocks)) {
+	                printf("socket %d was ready\n", s);
+	                if (s == sockfd) {
+	                    /* New connection */
+	                    int newsockfd;
+	                    struct sockaddr_in their_addr;
+	                    socklen_t size = sizeof(struct sockaddr_in);
+	                    newsockfd = accept(sockfd, (struct sockaddr*)&their_addr, &size);
+	                    if (newsockfd == -1) {
+	                        perror("accept");
+	                    } else {
+	                        printf("Got a connection from %s on port %d\n",
+	                                inet_ntoa(their_addr.sin_addr), htons(their_addr.sin_port));
+	                        FD_SET(newsockfd, &socks);
+	                        if (newsockfd > maxsock) {
+	                            maxsock = newsockfd;
+	                        }
+	                    }
+	                } else {
+	                    /* Handle read or disconnection */
+	                    handle(s, &socks);
+	                }
+	            }
+	        }
+
+	}
+
+	close(sockfd);
+
+	return 0;
+}
+
+void handle(int newsock, fd_set *set)
+{
+	char buffer[BUF_SIZE] = "Hello, World!";
+	send(newsock, buffer, BUF_SIZE, 0);
+	FD_CLR(newsock, set);
+	close(newsock);
+}
 
